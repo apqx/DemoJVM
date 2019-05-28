2602885c3ed6b07f065c81fd67c0e4d139e8b137
apqx <changgongapq@gmail.com>
2019-05-28 22:21:37 +0800

    指数计算练习

--- src/main/java/me/apqx/Demo.java
+++ src/main/java/me/apqx/Demo.java
@@ -1,50 +1,31 @@
 package me.apqx;
 
 
-import io.reactivex.*;
-import io.reactivex.disposables.Disposable;
-import me.apqx.util.Tools;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 public class Demo {
-    private static Disposable disposable;
-
-    public static void main(String[] args) throws Exception {
-        System.out.println(Tools.getMD5("900100001577810f47-ab78-4c21-b1be-262f7a176c72"));
-        System.out.println(Thread.currentThread());
-        Flowable.range(0, 10)
-                .subscribe(new Subscriber<Integer>() {
-                    Subscription sub;
-                    @Override
-                    public void onSubscribe(Subscription s) {
-                        System.out.println("onSubscribe");
-                        sub =s;
-                        s.request(1);
-                    }
-
-                    @Override
-                    public void onNext(Integer integer) {
-                        System.out.println("onNext " + integer);
-                        sub.request(1);
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        System.out.println("onComplete ");
-                    }
-                });
-        Thread.sleep(10000);
-    }
-
-    private void doSth() {
-
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Math.exp(i));
+        }
+        System.out.println(1e2);
     }
 
 
+
+}
+class Tools {
+    private Object object;
+
+    private Man man = new Man(object);
+
+    public Tools() {
+        object = "3";
+    }
+}
+
+class Man {
+    public Man(Object object) {
+        System.out.println(object == null);
+    }
 }
